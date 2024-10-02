a09469727397083281f352bf2b0a213bea609f72
Sami <sami.achbani@gmail.com>
2024-10-02 15:23:46 +0200

    Ajout de Bootstrapt js structure page comparateur

--- views/comparateur.php
+++ views/comparateur.php
@@ -2,7 +2,32 @@
     $strPage = "comparateur";
     $strTitre = "Comparateur Mansory";
     include("_partial/header.php");
-?>
+?> 
+
+    <div class="container">
+        <div class="row">
+            <div class="compar_gauche col-6 d-flex flex-column">
+
+
+
+                <div>gauche</div>
+                <div class="modal-dialog modal-xl">Quelque chose</div>
+
+
+
+
+            </div>
+            <div class="compar_droite col-6 d-flex flex-column">
+
+
+
+                <div>droite</div>
+
+
+
+            </div>  
+        </div>
+    </div>
 
 <?php 
     include("_partial/footer.php");
