1ced0dc42cb1d7aefc2bb025c0d8f90a9e6e556c
Sami <sami.achbani@gmail.com>
2024-10-02 17:00:17 +0200

    Ajout des accordéons et des cards à l'interieur dans la page comparateur

--- views/comparateur.php
+++ views/comparateur.php
@@ -3,32 +3,137 @@
     $strTitre = "Comparateur Mansory";
     include("_partial/header.php");
 ?> 
+    <main>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="/mansory_php/index.php">Accueil</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Comparateur</li>
+            </ol>
+        </nav>
+        <div class="container">
+            <div class="row justify-content-around">
 
-    <div class="container">
-        <div class="row">
-            <div class="compar_gauche col-6 d-flex flex-column">
+                <div class="compar_gauche col-12 col-lg-5 d-flex flex-column mt-5 p-2">
 
+                    <div class="compar_gauche_titre d-flex justify-content-center pb-5">Choisir la première voiture</div>
 
+                    <div class="accordion" id="accordionExample1">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne1" aria-expanded="true" aria-controls="collapseOne1">
+                                Accordion Item #1
+                            </button>
+                            </h2>
+                            <div id="collapseOne1" class="accordion-collapse collapse" data-bs-parent="#accordionExample1">
+                            <div class="accordion-body  d-flex justify-content-around">
+                                <div class="card" style="width: 15rem;">
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                                <div class="card" style="width: 15rem;">
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo1" aria-expanded="false" aria-controls="collapseTwo1">
+                                Accordion Item #2
+                            </button>
+                            </h2>
+                            <div id="collapseTwo1" class="accordion-collapse collapse" data-bs-parent="#accordionExample1">
+                            <div class="accordion-body">
+                                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree1" aria-expanded="false" aria-controls="collapseThree1">
+                                Accordion Item #3
+                            </button>
+                            </h2>
+                            <div id="collapseThree1" class="accordion-collapse collapse" data-bs-parent="#accordionExample1">
+                            <div class="accordion-body">
+                                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            </div>
+                            </div>
+                        </div>
+                    </div>
 
-                <div>gauche</div>
-                <div class="modal-dialog modal-xl">Quelque chose</div>
+                </div>
+                <div class="compar_droite col-12 col-lg-5 d-flex flex-column mt-5 p-2">
+                    
+                    <div class="compar_droite_titre d-flex justify-content-center pb-5">Choisir la deuxième voiture</div>
 
+                    <div class="accordion" id="accordionExample2">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne2" aria-expanded="true" aria-controls="collapseOne2">
+                                Accordion Item #1
+                            </button>
+                            </h2>
+                            <div id="collapseOne2" class="accordion-collapse collapse" data-bs-parent="#accordionExample2">
+                            <div class="accordion-body d-flex justify-content-around">
+                                <div class="card" style="width: 15rem;">
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                                <div class="card" style="width: 15rem;">
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo2" aria-expanded="false" aria-controls="collapseTwo2">
+                                Accordion Item #2
+                            </button>
+                            </h2>
+                            <div id="collapseTwo2" class="accordion-collapse collapse" data-bs-parent="#accordionExample2">
+                            <div class="accordion-body">
+                                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree2" aria-expanded="false" aria-controls="collapseThree2">
+                                Accordion Item #3
+                            </button>
+                            </h2>
+                            <div id="collapseThree2" class="accordion-collapse collapse" data-bs-parent="#accordionExample2">
+                            <div class="accordion-body">
+                                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            </div>
+                            </div>
+                        </div>
+                    </div>
 
-
-
+                </div>  
             </div>
-            <div class="compar_droite col-6 d-flex flex-column">
-
-
-
-                <div>droite</div>
-
-
-
-            </div>  
         </div>
-    </div>
-
+    </main>
 <?php 
     include("_partial/footer.php");
 ?>
