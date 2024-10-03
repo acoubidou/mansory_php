eacd498c6d576e549617b894b95d9fb22efed365
Sami <sami.achbani@gmail.com>
2024-10-03 14:38:09 +0200

    Je sais pas ce que j'ai fait

--- views/comparateur.php
+++ views/comparateur.php
@@ -12,29 +12,29 @@
         </nav>
         <div class="container">
             <div class="row justify-content-around">
-                <div class="compar_gauche col-12 col-lg-5 d-flex flex-column mt-5 p-2">
+                <div class="col-12 col-lg-5 d-flex flex-column mt-5 p-2">
                     <div class="compar_gauche_titre d-flex justify-content-center pb-5">Choisir la première voiture</div>
 
                     <div class="accordion pb-5" id="accordionExample1">
 
                         <?php
                             $array = [
-                                ["1","1001","17","17.1","Aston Martin","DBX","aston_martin","dbx","Rien","Rien","Rien"],
-                                ["2","1002","18","18.1","Audi","RS6","audi","RS6","RSQ8","audi","rsq8"],
-                                ["3","1003","19","19.1","Bentley","BENTAYGA","bentley","bentayga","FLYING SPUR","bentley","flying_spur"],
-                                ["4","1004","20","20.1","BMW","SERIE 7","bmw","serie7","SERIE 7 NEW","bmw","serie7new"],
-                                ["5","1005","21","21.1","Bugatti","CHIRON","bugatti","chiron","Rien","Rien","Rien"],
-                                ["6","1006","22","22.1","Cadillac","ESCALADE","cadillac","escalade","Rien","Rien","Rien"],
-                                ["7","1007","23","23.1","Ferrari","812 GTS","ferrari","812_gts","SF90 SPIDER","ferrari","sf90_spider"],
-                                ["8","1008","24","24.1","Ford","GT","ford","gt","Rien","Rien","Rien"],
-                                ["9","1009","25","25.1","Lamborghini","URUS","lamborghini","urus","AVENTADOR SVJ ROADSTER","lamborghini","aventador_svj_roadster"],
-                                ["10","1010","26","26.1","Land Rover","RANGE ROVER SV","land_rover","sv","Rien","Rien","Rien"],
-                                ["11","1011","27","27.1","Lotus","EVORA GTE","lotus","evora_gte","Rien","Rien","Rien"],
-                                ["12","1012","28","28.1","Maserati","MC20","maserati","mc20","Rien","Rien","Rien"],
-                                ["13","1013","29","29.1","Mclaren","720S","mclaren","720S","Rien","Rien","Rien"],
-                                ["14","1014","30","30.1","Mercedes","AMG GT63 S E PERFORMANCE","mercedes","amg_gt63_s_e_performance","SL63","mercedes","sl63",],
-                                ["15","1015","31","31.1","Porsche","911 (992)","porsche","911_(992)","Rien","Rien","Rien"],
-                                ["16","1016","32","32.1","Rolls-Royce","CULLINAN","rolls_royce","cullinan","GHOST","rolls_royce","ghost"],
+                                ["1","101","17","17.1","Aston Martin","DBX","aston_martin","dbx","Rien","Rien","Rien"],
+                                ["2","102","18","18.1","Audi","RS6","audi","RS6","RSQ8","audi","rsq8"],
+                                ["3","103","19","19.1","Bentley","BENTAYGA","bentley","bentayga","FLYING SPUR","bentley","flying_spur"],
+                                ["4","104","20","20.1","BMW","SERIE 7","bmw","serie7","SERIE 7 NEW","bmw","serie7new"],
+                                ["5","105","21","21.1","Bugatti","CHIRON","bugatti","chiron","Rien","Rien","Rien"],
+                                ["6","106","22","22.1","Cadillac","ESCALADE","cadillac","escalade","Rien","Rien","Rien"],
+                                ["7","107","23","23.1","Ferrari","812 GTS","ferrari","812_gts","SF90 SPIDER","ferrari","sf90_spider"],
+                                ["8","108","24","24.1","Ford","GT","ford","gt","Rien","Rien","Rien"],
+                                ["9","109","25","25.1","Lamborghini","URUS","lamborghini","urus","AVENTADOR SVJ ROADSTER","lamborghini","aventador_svj_roadster"],
+                                ["10","110","26","26.1","Land Rover","RANGE ROVER SV","land_rover","sv","Rien","Rien","Rien"],
+                                ["11","111","27","27.1","Lotus","EVORA GTE","lotus","evora_gte","Rien","Rien","Rien"],
+                                ["12","112","28","28.1","Maserati","MC20","maserati","mc20","Rien","Rien","Rien"],
+                                ["13","113","29","29.1","Mclaren","720S","mclaren","720S","Rien","Rien","Rien"],
+                                ["14","114","30","30.1","Mercedes","AMG GT63 S E PERFORMANCE","mercedes","amg_gt63_s_e_performance","SL63","mercedes","sl63",],
+                                ["15","115","31","31.1","Porsche","911 (992)","porsche","911_(992)","Rien","Rien","Rien"],
+                                ["16","116","32","32.1","Rolls-Royce","CULLINAN","rolls_royce","cullinan","GHOST","rolls_royce","ghost"],
                             ];
 
                             foreach ($array as list($numero1, $sousnumero1, $numero2, $sousnumero2, $marque, $modele, $marque_lien, $modele_lien, $modele2, $marque_lien2, $modele_lien2)) {
@@ -47,7 +47,7 @@
                                         </button>
                                         </h2>
                                         <div id="collapse'.$numero1.'" class="accordion-collapse collapse" data-bs-parent="#accordionExample1">
-                                            <div class="accordion-body d-flex justify-content-around">
+                                            <div class="accordion-body d-flex justify-content-center">
                                                 <div class="card me-2" style="width: 15rem;">
                                                     <img src="/mansory_php/assets/images/voitures/'.$marque_lien.'/'.$modele_lien.'/'.$modele_lien.'_avant.jpg" class="card-img-top" alt="'.$modele.'">
                                                     <div class="card-body">
@@ -77,9 +77,9 @@
                         ?>
                     </div>
                 </div>
+
                 
-                
-                <div class="compar_droite col-12 col-lg-5 d-flex flex-column mt-5 p-2">
+                <div class="col-12 col-lg-5 d-flex flex-column mt-5 p-2">
                     
                     <div class="compar_droite_titre d-flex justify-content-center pb-5">Choisir la deuxième voiture</div>
 
@@ -131,6 +131,21 @@
 
 
             </div>
+
+            <div class="row justify-content-around">
+                <div class="compar_gauche col-6 d-flex flex-column">
+                    <div class="d-flex justify-content-center">
+                        <p>ici</p>
+                    </div>
+
+                </div>
+                <div class="col-6 d-flex flex-column">
+                    <div class="compar_droite d-flex justify-content-center">
+                        <p>ici</p>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </main>
 <?php 
